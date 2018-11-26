9f7d5efd045981af38a6c3f25f83a52f96ea327d
DecibelM <42909365+DecibelM@users.noreply.github.com>
2018-11-26 14:45:26 +0100

    Initial Commit

---
+++ src/SLCWithGet.java
@@ -0,0 +1,19 @@
+public class SLCWithGet<E extends Comparable<? super E>>
+        extends LinkedCollection<E>
+        implements CollectionWithGet<E> {
+
+
+
+    @Override
+    public E get(E e) {
+        return null;
+    }
+}
+
+
+//Done: Created class SLCWithGet (this class). Class head done by looking at picture
+// and comparing to BSTwithGet class.
+
+//TODO: Write method add, LinkedCollection is not sorted. Least element should be first.
+//TODO: How to know what element is least?
+//TODO: Write method get.
