ed571f0a624dce8dda5321e9f04ec725c327905d
DecibelM <42909365+DecibelM@users.noreply.github.com>
2018-11-27 16:33:11 +0100

    Work done 181127

--- src/SplayWithGet.java
+++ src/SplayWithGet.java
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 public class SplayWithGet<E extends Comparable<? super E>>
         extends BinarySearchTree<E>
-        implements CollectionWithGet<E>{
+        implements CollectionWithGet<E> {
 
     public SplayWithGet() {
         super();
     }
 
     public E get(E e) {
-        Entry t = find(e,root);
+        Entry t = find(e, root);
         return t == null ? null : t.element;
     }  // get
 
@@ -20,19 +19,19 @@ public class SplayWithGet<E extends Comparable<? super E>>
             / \                    / \
            A   B                  B   C
      */
-    private void zig( Entry x ) {
-        Entry   y = x.left;
-        E    temp = x.element;
+    private void zig(Entry x) {
+        Entry y = x.left;
+        E temp = x.element;
         x.element = y.element;
         y.element = temp;
-        x.left    = y.left;
-        if ( x.left != null )
-            x.left.parent   = x;
-        y.left    = y.right;
-        y.right   = x.right;
-        if ( y.right != null )
-            y.right.parent  = y;
-        x.right   = y;
+        x.left = y.left;
+        if (x.left != null)
+            x.left.parent = x;
+        y.left = y.right;
+        y.right = x.right;
+        if (y.right != null)
+            y.right.parent = y;
+        x.right = y;
     } //   zig
     // ========== ========== ========== ==========
 
@@ -43,19 +42,19 @@ public class SplayWithGet<E extends Comparable<? super E>>
                / \            / \
               B   C          A   B
     */
-    private void zag( Entry x ) {
-        Entry  y  = x.right;
-        E temp    = x.element;
+    private void zag(Entry x) {
+        Entry y = x.right;
+        E temp = x.element;
         x.element = y.element;
         y.element = temp;
-        x.right   = y.right;
-        if ( x.right != null )
-            x.right.parent  = x;
-        y.right   = y.left;
-        y.left    = x.left;
-        if ( y.left != null )
-            y.left.parent   = y;
-        x.left    = y;
+        x.right = y.right;
+        if (x.right != null)
+            x.right.parent = x;
+        y.right = y.left;
+        y.left = x.left;
+        if (y.left != null)
+            y.left.parent = y;
+        x.left = y;
     } //   zag
     // ========== ========== ========== ==========
 
@@ -68,21 +67,21 @@ public class SplayWithGet<E extends Comparable<? super E>>
              / \
             B   C
     */
-    private void zigzag( Entry x ) {
-        Entry   y = x.left,
+    private void zigzag(Entry x) {
+        Entry y = x.left,
                 z = x.left.right;
-        E       e = x.element;
+        E e = x.element;
         x.element = z.element;
         z.element = e;
-        y.right   = z.left;
-        if ( y.right != null )
+        y.right = z.left;
+        if (y.right != null)
             y.right.parent = y;
-        z.left    = z.right;
-        z.right   = x.right;
-        if ( z.right != null )
+        z.left = z.right;
+        z.right = x.right;
+        if (z.right != null)
             z.right.parent = z;
-        x.right   = z;
-        z.parent  = x;
+        x.right = z;
+        z.parent = x;
     }  //  zigzag
     // ========== ========== ========== ==========
 
@@ -95,85 +94,81 @@ public class SplayWithGet<E extends Comparable<? super E>>
               / \
              B   C
      */
-    private void zagzig( Entry x ) {
-        Entry  y  = x.right,
-                z  = x.right.left;
-        E      e  = x.element;
+    private void zagzig(Entry x) {
+        Entry y = x.right,
+                z = x.right.left;
+        E e = x.element;
         x.element = z.element;
         z.element = e;
-        y.left    = z.right;
-        if ( y.left != null )
+        y.left = z.right;
+        if (y.left != null)
             y.left.parent = y;
-        z.right   = z.left;
-        z.left    = x.left;
-        if ( z.left != null )
+        z.right = z.left;
+        z.left = x.left;
+        if (z.left != null)
             z.left.parent = z;
-        x.left    = z;
-        z.parent  = x;
+        x.left = z;
+        z.parent = x;
     } //  zagzig
-
-
-
-
 
 
     private void zigzig(Entry x) {
         Entry y = x.left,
                 z = x.left.left;
-        E       e = x.element;
+        E e = x.element;
         x.element = z.element;
         z.element = e;
 
 
         Entry t = z.right;
         z.right = x.left;
-        if (z.right != null){
+        if (z.right != null) {
             z.right.parent = z;
         }
         y.left = t;
-        if (y.left != null){
+        if (y.left != null) {
             y.left.parent = y;
         }
         t = y.right;
         y.right = x;
-        if (y.right != null){
+        if (y.right != null) {
             z.parent = x.parent;
             y.right.parent = y;
         }
         x.left = t;
-        if (x.left != null){
+        if (x.left != null) {
             x.left.parent = x;
         }
 
     } // zigzig
 
 
-
     private void zagzag(Entry x) {
         Entry y = x.right,
                 z = x.right.right;
-        E       e = x.element;
+        E e = x.element;
         x.element = z.element;
         z.element = e;
 
 
         Entry t = z.left;
         z.left = x.right;
-        if (z.left != null){
+        if (z.left != null) {
             z.left.parent = z;
         }
         y.right = t;
-        if (y.right != null){
+        if (y.right != null) {
             y.right.parent = y;
         }
         t = y.left;
         y.left = x;
-        if (y.left != null){
+        if (y.left != null) {
             z.parent = x.parent;
             y.left.parent = y;
         }
         x.right = t;
-        if (x.right != null){
+        if (x.right != null) {
             x.right.parent = x;
         }
     } // zagzag
+}
