9ac83dd8d5190cd525744f153232cb067e2ca952
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2018-11-27 13:43:42 +0100

    Test av commit på ITellijae

--- src/SLCWithGet.java
+++ src/SLCWithGet.java
@@ -5,10 +5,29 @@ public class SLCWithGet<E extends Comparable<? super E>>
 
 
     @Override
-    public E get(E e) {
+    public E get(E element) {
+        Entry t = head;
+        while (t != null) {
+            if (t.element == element) {
+                return t.element;
+            } else if (t != element) {
+                t = t.next;
+            }
+        }
         return null;
     }
+
+
+    @Override
+    public boolean add(E e){
+
+
+
+        return true;
+    }
 }
+
+
 
 
 //Done: Created class SLCWithGet (this class). Class head done by looking at picture
